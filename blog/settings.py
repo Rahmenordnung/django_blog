3b40f418bf30e84dc325a4a1e7c485b136f5bd06
Tudor George <tudorpascu787@gmail.com>
2019-05-15 11:31:55 +0000

    trying to deploy to heroku, correcting typos

--- blog/settings.py
+++ blog/settings.py
@@ -78,18 +78,19 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-if "DATABASES_URL" in os.environ:
+
+if "DATABASE_URL" in os.environ:
     DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
-    print("postgresURL NOT FOUND USING sQL lITE INSTEAD")
+    print("Postgres URL not found, using sqlite instead")
     DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
 
 
 # Password validation
